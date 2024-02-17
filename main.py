6f001234b9452bee3ecb5ad9f6ce72252abdd9cf
Diablo Redfox <72261670+DlABLO-REDFOX@users.noreply.github.com>
2024-02-17 13:11:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,111 +21,6 @@ password_img = CTkImage(dark_image=password_img_data, light_image=password_img_d
 side_img = CTkImage(dark_image=side_img_data, light_image=side_img_data, size=(300, 300))
 logo_img = CTkImage(dark_image=logo_img_data, light_image=logo_img_data, size=(50, 50))
 
-# GUI Code
-# window
-window = CTk()
-window.title('Study Material Repository')
-window.geometry('600x500')
-window.minsize(600, 500)
-set_appearance_mode('light')
-
-# Login page
-login_page = CTkFrame(master=window)
-login_page.pack(expand=True, fill='both')
-side_img_label = CTkLabel(master=login_page, text="", image=side_img)
-side_img_label.pack(expand=True, side="left")
-
-right_frame = CTkFrame(master=login_page, height=500, width=300, fg_color="#ffffff")
-right_frame.pack(expand=True, side='right', fill='both')
-
-login_frame = CTkFrame(master=right_frame, height=500, width=300, fg_color="#ffffff")
-login_frame.propagate(False)
-login_frame.pack(expand=True)
-
-logo_img_label = CTkLabel(master=login_frame, text="", image=logo_img)
-logo_img_label.pack(pady=10)
-
-title_label = CTkLabel(master=login_frame, text='Log in', text_color='#1f61a5', anchor='w', justify='left',
-                       font=("Arial Bold", 24))
-title_label.pack(anchor="w", pady=(50, 5), padx=(25, 0))
-
-id_label = CTkLabel(master=login_frame, text='  User ID', text_color="#1f61a5", anchor="w", justify="left",
-                    font=("Arial Bold", 14), image=user_img, compound="left")
-id_label.pack(anchor="w", pady=(38, 0), padx=(25, 0))
-id_entry = CTkEntry(master=login_frame, width=225, fg_color="#EEEEEE", border_color="#1f61a5", border_width=1,
-                    text_color="#000000")
-id_entry.pack(anchor="w", padx=(25, 0))
-
-password_label = CTkLabel(master=login_frame, text='  Password', text_color="#1f61a5", anchor="w", justify="left",
-                          font=("Arial Bold", 14), image=password_img, compound="left")
-password_label.pack(anchor="w", pady=(38, 0), padx=(25, 0))
-password_entry = CTkEntry(master=login_frame, show='*', width=225, fg_color="#EEEEEE", border_color="#1f61a5",
-                          border_width=1, text_color="#000000")
-password_entry.pack(anchor="w", padx=(25, 0))
-
-error_label = CTkLabel(master=login_frame, text="", text_color="#FF0000")
-error_label.pack(anchor="w", padx=(25, 0))
-
-login_button = CTkButton(master=login_frame, text="Login", fg_color="#1f61a5", hover_color="#19429d",
-                         font=("Arial Bold", 12), text_color="#ffffff", width=225, command=lambda: login())
-login_button.pack(anchor="w", pady=(40, 0), padx=(25, 0))
-
-# Main Menu
-main_menu = CTkFrame(master=window)
-
-# Buttons menu
-main_menu_title = CTkLabel(master=main_menu, text="Choose Tab", text_color='#1f61a5', anchor='w', justify='left',
-                           font=("Arial Bold", 24))
-main_menu_title.pack()
-
-buttons_menu = CTkFrame(master=main_menu)
-buttons_menu.pack(pady=20)
-
-notes_button = CTkButton(master=buttons_menu, text='Notes', command=lambda: show_tabs('Notes'))
-notes_button.pack(pady=20, padx=15)
-qb_button = CTkButton(master=buttons_menu, text='Question Banks', command=lambda: show_tabs('Question Banks'))
-qb_button.pack(pady=20, padx=15)
-qna_button = CTkButton(master=buttons_menu, text='Q & A', command=lambda: show_tabs('Q & A'))
-qna_button.pack(pady=20, padx=15)
-
-# Tabs
-menu_tabs = CTkTabview(master=window)
-
-notes_tab = menu_tabs.add('Notes')
-qb_tab = menu_tabs.add('Question Banks')
-qna_tab = menu_tabs.add('Q & A')
-
-# Notes tab
-note_dropdown_menu = CTkFrame(master=notes_tab, height=500, width=200)
-note_dropdown_menu.pack(side='left')
-
-courses = ['', 'B.Tech', 'Other']
-branches = ['', 'CSE', 'Other']
-years = ['', '1st year', '2nd year', '3rd year', '4th year']
-subjects = ['']
-modules = ['', '1', '2', '3', '4', '5']
-
-courses_box = CTkComboBox(master=note_dropdown_menu, values=courses).pack(pady=20, padx=10)
-branches_box = CTkComboBox(master=note_dropdown_menu, values=branches).pack(pady=20, padx=10)
-years_box = CTkComboBox(master=note_dropdown_menu, values=years).pack(pady=20, padx=10)
-subjects_box = CTkComboBox(master=note_dropdown_menu, values=subjects).pack(pady=20, padx=10)
-modules_box = CTkComboBox(master=note_dropdown_menu, values=modules).pack(pady=20, padx=10)
-
-search_button = CTkButton(master=note_dropdown_menu, text='Search').pack(pady=20)
-
-# qb tab
-qb_dropdown_menu = CTkFrame(master=qb_tab, height=500, width=200)
-qb_dropdown_menu.pack(side='left')
-qb_courses_box = CTkComboBox(master=qb_dropdown_menu, values=courses).pack(pady=20, padx=10)
-qb_branches_box = CTkComboBox(master=qb_dropdown_menu, values=branches).pack(pady=20, padx=10)
-qb_years_box = CTkComboBox(master=qb_dropdown_menu, values=years).pack(pady=20, padx=10)
-qb_subjects_box = CTkComboBox(master=qb_dropdown_menu, values=subjects).pack(pady=20, padx=10)
-qb_modules_box = CTkComboBox(master=qb_dropdown_menu, values=modules).pack(pady=20, padx=10)
-
-qb_search_button = CTkButton(master=qb_dropdown_menu, text='Search').pack(pady=20)
-
-# run
-window.mainloop()
 
 def user_details_add():
     flag = 0
@@ -245,7 +140,7 @@ def download_pdf(pdf_id):
 
 
 #  pdfs_id = "ZnceNJGoYhUflbKxMrpX"
-#  download_pdf(pdfs_id)  this is how it works btw
+#  download_pdf(pdfs_id)
 
 def show_tabs(tab_name):
     menu_tabs.pack(expand=True, fill='both')
@@ -260,3 +155,109 @@ def next_page(current_page, next_page_frame):
 
 def set_text(text):  # function to set error text
     error_label.configure(text=text)
+
+# GUI Code
+# window
+window = CTk()
+window.title('Study Material Repository')
+window.geometry('600x500')
+window.minsize(600, 500)
+set_appearance_mode('light')
+
+# Login page
+login_page = CTkFrame(master=window)
+login_page.pack(expand=True, fill='both')
+side_img_label = CTkLabel(master=login_page, text="", image=side_img)
+side_img_label.pack(expand=True, side="left")
+
+right_frame = CTkFrame(master=login_page, height=500, width=300, fg_color="#ffffff")
+right_frame.pack(expand=True, side='right', fill='both')
+
+login_frame = CTkFrame(master=right_frame, height=500, width=300, fg_color="#ffffff")
+login_frame.propagate(False)
+login_frame.pack(expand=True)
+
+logo_img_label = CTkLabel(master=login_frame, text="", image=logo_img)
+logo_img_label.pack(pady=10)
+
+title_label = CTkLabel(master=login_frame, text='Log in', text_color='#1f61a5', anchor='w', justify='left',
+                       font=("Arial Bold", 24))
+title_label.pack(anchor="w", pady=(50, 5), padx=(25, 0))
+
+id_label = CTkLabel(master=login_frame, text='  User ID', text_color="#1f61a5", anchor="w", justify="left",
+                    font=("Arial Bold", 14), image=user_img, compound="left")
+id_label.pack(anchor="w", pady=(38, 0), padx=(25, 0))
+id_entry = CTkEntry(master=login_frame, width=225, fg_color="#EEEEEE", border_color="#1f61a5", border_width=1,
+                    text_color="#000000")
+id_entry.pack(anchor="w", padx=(25, 0))
+
+password_label = CTkLabel(master=login_frame, text='  Password', text_color="#1f61a5", anchor="w", justify="left",
+                          font=("Arial Bold", 14), image=password_img, compound="left")
+password_label.pack(anchor="w", pady=(38, 0), padx=(25, 0))
+password_entry = CTkEntry(master=login_frame, show='*', width=225, fg_color="#EEEEEE", border_color="#1f61a5",
+                          border_width=1, text_color="#000000")
+password_entry.pack(anchor="w", padx=(25, 0))
+
+error_label = CTkLabel(master=login_frame, text="", text_color="#FF0000")
+error_label.pack(anchor="w", padx=(25, 0))
+
+login_button = CTkButton(master=login_frame, text="Login", fg_color="#1f61a5", hover_color="#19429d",
+                         font=("Arial Bold", 12), text_color="#ffffff", width=225, command=lambda: login())
+login_button.pack(anchor="w", pady=(40, 0), padx=(25, 0))
+
+# Main Menu
+main_menu = CTkFrame(master=window)
+
+# Buttons menu
+main_menu_title = CTkLabel(master=main_menu, text="Choose Tab", text_color='#1f61a5', anchor='w', justify='left',
+                           font=("Arial Bold", 24))
+main_menu_title.pack()
+
+buttons_menu = CTkFrame(master=main_menu)
+buttons_menu.pack(pady=20)
+
+notes_button = CTkButton(master=buttons_menu, text='Notes', command=lambda: show_tabs('Notes'))
+notes_button.pack(pady=20, padx=15)
+qb_button = CTkButton(master=buttons_menu, text='Question Banks', command=lambda: show_tabs('Question Banks'))
+qb_button.pack(pady=20, padx=15)
+qna_button = CTkButton(master=buttons_menu, text='Q & A', command=lambda: show_tabs('Q & A'))
+qna_button.pack(pady=20, padx=15)
+
+# Tabs
+menu_tabs = CTkTabview(master=window)
+
+notes_tab = menu_tabs.add('Notes')
+qb_tab = menu_tabs.add('Question Banks')
+qna_tab = menu_tabs.add('Q & A')
+
+# Notes tab
+note_dropdown_menu = CTkFrame(master=notes_tab, height=500, width=200)
+note_dropdown_menu.pack(side='left')
+
+courses = ['', 'B.Tech', 'Other']
+branches = ['', 'CSE', 'Other']
+years = ['', '1st year', '2nd year', '3rd year', '4th year']
+subjects = ['']
+modules = ['', '1', '2', '3', '4', '5']
+
+courses_box = CTkComboBox(master=note_dropdown_menu, values=courses).pack(pady=20, padx=10)
+branches_box = CTkComboBox(master=note_dropdown_menu, values=branches).pack(pady=20, padx=10)
+years_box = CTkComboBox(master=note_dropdown_menu, values=years).pack(pady=20, padx=10)
+subjects_box = CTkComboBox(master=note_dropdown_menu, values=subjects).pack(pady=20, padx=10)
+modules_box = CTkComboBox(master=note_dropdown_menu, values=modules).pack(pady=20, padx=10)
+
+search_button = CTkButton(master=note_dropdown_menu, text='Search').pack(pady=20)
+
+# qb tab
+qb_dropdown_menu = CTkFrame(master=qb_tab, height=500, width=200)
+qb_dropdown_menu.pack(side='left')
+qb_courses_box = CTkComboBox(master=qb_dropdown_menu, values=courses).pack(pady=20, padx=10)
+qb_branches_box = CTkComboBox(master=qb_dropdown_menu, values=branches).pack(pady=20, padx=10)
+qb_years_box = CTkComboBox(master=qb_dropdown_menu, values=years).pack(pady=20, padx=10)
+qb_subjects_box = CTkComboBox(master=qb_dropdown_menu, values=subjects).pack(pady=20, padx=10)
+qb_modules_box = CTkComboBox(master=qb_dropdown_menu, values=modules).pack(pady=20, padx=10)
+
+qb_search_button = CTkButton(master=qb_dropdown_menu, text='Search').pack(pady=20)
+
+# run
+window.mainloop()
