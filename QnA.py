b698b692fcfee9db974e9ab78a49287c57775057
HaroonRasheed1903 <99037171+HaroonRasheed1903@users.noreply.github.com>
2024-03-25 18:53:06 +0530

    Update QnA.py

--- QnA.py
+++ QnA.py
@@ -1,9 +1,9 @@
-import customtkinter as ct
+from customtkinter import *
 import tkinter as tk
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate(r"D:\Athul\Miniproject\Main_prgm\Firebase_PvtKey.json")
+cred = credentials.Certificate(r"C:\Users\Admin\PycharmProjects\MiniProject\study-material-repo-firebase-adminsdk-bj0om-7cddb30570.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 collection_ref = db.collection('qna')
@@ -19,10 +19,10 @@ def empty_frame(frame):  # Empties a frame
 
 # Example usage
 def answer_gui(rframe):  # function to show answer in GUI....needs modification
-    dialog = ct.CTkInputDialog(text="Enter data:", title="Test")
+    dialog = CTkInputDialog(text="Enter data:", title="Test")
     data = dialog.get_input()
     if data:
-        ct.CTkLabel(master=rframe)
+        CTkLabel(master=rframe)
 
 
 def getqns():  # gets qns and stores them in dict_qns and list_qns..dictids has id and serial number
@@ -79,68 +79,84 @@ def answer_qn():  # Answer a qn
     # print(ans)
 
 
-def fetch_data(rframe, qn):  # fetches data to put in rframe..qns and answers
+def fetch_data(qn):  # fetches data to put in rframe..qns and answers
     # print(rframe)
     # print(qn)
-    print(dict_qns)
-    empty_frame(rframe)
+    empty_frame(raframe)
     for id, q in dict_qns.items():
         if q == qn:
             break
 
-    ct.CTkLabel(master=rframe, width=600, text=qn, anchor='w').pack(pady=10, padx=(30, 0))
+    rqframe.configure(text=qn)
     ans = get_ans(id)
     if ans is None:
-        ct.CTkLabel(master=rframe,width=600,text='No Answer Yet',text_color='#ff0000',anchor='e').pack(pady=10,padx=(0,30))
+        CTkLabel(master=raframe, text='No Answer Yet',text_color='#ff0000',anchor='e').pack(padx=10,pady=10)
     else:
         for i in ans:
-            print(i)
-            ct.CTkLabel(master=rframe, width=600, text=i, anchor='e').pack(pady=10, padx=10)
+            message_frame = CTkFrame(master=raframe,fg_color='#e4e5f1')
+            message_frame.pack(padx=5,pady=5,side=TOP, anchor=NW)
+            CTkLabel(master=message_frame, text=i, anchor='e').pack(padx=10,pady=5)
 
 
-def main():
+def test():
+    frame = CTkFrame(master=root)
+    frame.pack()
+
+# Main GUI
+root = CTk(fg_color='#fafafa')
+root.title('Hello')
+root.geometry('800x500')
+root.minsize(800, 500)
+set_appearance_mode('light')
+
+def button_click_event():
+    # buttons in scrolling frame and their event handling
     global r
-    # Main GUI
-    root = ct.CTk()
-    root.title('Hello')
-    root.geometry('500x600')
-    root.minsize(500, 600)
+    dialog = CTkInputDialog(text="Enter Question:", title="Test")
+    data = dialog.get_input()
+    if data:
+        frame = CTkFrame(master=scrolling_frame_1, fg_color='#e4e5f1')
+        frame.pack(fill='x', pady=5, padx=5)
+        CTkLabel(master=frame, text=data, anchor='w',text_color="#1f61a5", justify="left",
+                          font=("Arial Bold", 14)).pack(side='left', padx=10, pady=10)
+        b = CTkButton(master=frame, text='View', hover=True)
+        b.bind("<Button-1>", lambda event, text=data: fetch_data(text))
+        b.pack(side=RIGHT, padx=20, pady=5)
+        doc_ref = db.collection('qna').document()
+        doc_ref.set({'qn': data, 'ans': ''})
+        getqns()
+        r += 1
+post_button = CTkButton(master=root, text='Post Question', corner_radius=50, hover_color='#ff0000', hover=True,command=button_click_event)
+post_button.pack(pady=10)
+rframe = CTkFrame(master=root, width=400)
+rframe.pack(side='right', fill='both', expand=True)
+rqframe = CTkLabel(master=rframe, text="",fg_color='#fafafa',text_color="#1f61a5", justify="left",
+                          font=("Arial Bold", 14), width=400)
+rqframe.pack(fill='x')
+raframe = CTkScrollableFrame(master=rframe,fg_color='#fafafa')
+raframe.pack(expand=True,fill='both')
+ransbox = CTkFrame(master=rframe, width=400)
+ransbox.pack(fill='x')
+ranstextbox = CTkEntry(master=ransbox)
+ranstextbox.pack(side=LEFT, expand=True,fill='x',pady=5,padx=5)
+ransbutton = CTkButton(master=ransbox, text='Answer', command=lambda: answer_qn())
+ransbutton.pack(side=LEFT,pady=5,padx=5)
+scrolling_frame_1 = CTkScrollableFrame(master=root,width=400,
+                                              fg_color='#fafafa', scrollbar_button_color='#d2d3db')  # fg_color='#b2ccf2')
+scrolling_frame_1.pack(side='left', fill='both', expand=True)
+getqns()  # call to get/update qns
+for i in dict_ids:
+    q = dict_qns[dict_ids[i]]
+    frame = CTkFrame(master=scrolling_frame_1, fg_color='#e4e5f1')
+    frame.pack(fill='x', pady=5,padx=5)
+    CTkLabel(master=frame, text=q, anchor='w', text_color="#1f61a5", justify="left",
+                          font=("Arial Bold", 14)).pack(side='left', padx=10, pady=10)
 
-    def button_click_event():
-        # buttons in scrolling frame and their event handling
-        global r
-        print(r)
-        dialog = ct.CTkInputDialog(text="Enter Question:", title="Test")
-        data = dialog.get_input()
-        if data:
-            ct.CTkLabel(master=scrolling_frame_1, text=data, anchor='w').grid(row=r, column=0, padx=(100, 100), pady=5)
-            b = ct.CTkButton(master=scrolling_frame_1, text='View', fg_color='#aab7f2', hover_color='#99a4d9',
-                             hover=True)
-            b.bind("<Button-1>", lambda event, text=data: fetch_data(rframe, text))
-            b.grid(row=r, column=1, padx=20, pady=5)
-            doc_ref = db.collection('qna').document()
-            doc_ref.set({'qn': data, 'ans': ''})
-            getqns()
-            r += 1
-    post_button = ct.CTkButton(master=root, text='Post Question', corner_radius=50, hover_color='#ff0000', hover=True,command=button_click_event)
-    post_button.pack(pady=10)
-    rframe = ct.CTkFrame(master=root, height=500, width=600)
-    rframe.pack(side='right', fill='both')
-
-    scrolling_frame_1 = ct.CTkScrollableFrame(master=root, height=500, width=600,
-                                              scrollbar_button_hover_color='#aab7f2')  # fg_color='#b2ccf2')
-    scrolling_frame_1.pack(side='left', fill='y')
-    getqns()  # call to get/update qns
-
-    for i in dict_ids:
-        q = dict_qns[dict_ids[i]]
-        ct.CTkLabel(master=scrolling_frame_1, text=q, anchor='w').grid(row=r, column=0, padx=(100, 100), pady=10)
-        b = ct.CTkButton(master=scrolling_frame_1, text='View', fg_color='#aab7f2', hover_color='#99a4d9', hover=True)
-        b.bind("<Button-1>", lambda event, text=q: fetch_data(rframe, text))
-        b.grid(row=r, column=1, padx=20, pady=5)
-        r = r + 1
-    root.mainloop()
+    b = CTkButton(master=frame, text='View', hover=True)
+    b.bind("<Button-1>", lambda event, text=q: fetch_data(text))
+    b.pack(side=RIGHT, padx=20, pady=5)
+    b = CTkButton(master=scrolling_frame_1)
+    r = r + 1
+root.mainloop()
 
 
-if __name__ == '__main__':
-    main()
